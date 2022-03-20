c69b7832d0eef575c48694f32f33e9c33839a108
DoWhatYouL0ve <william_ross@mailfence.com>
2022-03-20 19:53:36 +0100

    eslintrc.js and prettierrc.js have been added and code has been adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-        <p>sdlkfgvnlkdfvmkl</p>
-    </div>
-  );
+    return <div className="App"></div>
 }
 
-export default App;
+export default App
