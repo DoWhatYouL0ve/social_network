16736a43b6c15372c8df1f107ec12b7c5838e273
DoWhatYouL0ve <william_ross@mailfence.com>
2022-04-02 21:38:11 +0200

    lesson11

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
 import React from 'react'
-import { Technologies } from './Technologies'
-import { Header } from './Header'
-import { Footer } from './Footer'
+import './App.css'
 
 const App = () => {
     return (
         <div className="App">
-            <Header />
-            <Technologies />
-            <Footer />
+            <header className="header">
+                <img
+                    src="https://image.shutterstock.com/image-vector/earth-globe-icon-trendy-logo-260nw-1257541729.jpg"
+                    alt="logo"
+                />
+            </header>
+            <nav className="nav">
+                <div>
+                    <a href="#">Profile</a>
+                </div>
+                <div>
+                    <a href="#">Messages</a>
+                </div>
+                <div>
+                    <a href="#">News</a>
+                </div>
+                <div>
+                    <a href="#">Music</a>
+                </div>
+                <div>
+                    <a href="#">Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img
+                        src="https://www.lovehappensmag.com/blog/wp-content/uploads/2016/06/Most-beautiful-beaches-slider-1.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>ava + description</div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                    <div>post 3</div>
+                </div>
+            </div>
         </div>
     )
 }
