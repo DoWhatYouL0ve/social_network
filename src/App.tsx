7922d91b7df3e68beda7c13d6d07d9b99525d104
DoWhatYouL0ve <william_ross@mailfence.com>
2022-04-02 20:04:35 +0200

    lesson11 footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Technologies } from './Technologies'
 import { Header } from './Header'
+import { Footer } from './Footer'
 
 const App = () => {
     return (
         <div className="App">
             <Header />
             <Technologies />
+            <Footer />
         </div>
     )
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+            <a href="#">Home </a>
+            <a href="#">News </a>
+            <a href="#">Messages</a>
+        </div>
+    )
+}
