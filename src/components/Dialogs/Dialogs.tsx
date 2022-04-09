6f0faee1ea29746a76641c8d12f73cf2f21196aa
DoWhatYouL0ve <william_ross@mailfence.com>
2022-04-09 18:51:08 +0200

    lesson21 page Dialogs CSS adjustment

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,5 +2,25 @@ import React from 'react'
 import style from './Dialogs.module.css'
 
 export const Dialogs = () => {
-    return <div>Dialogs</div>
+    return (
+        <div className={style.dialogsPageWrapper}>
+            <div className={style.dialogsSection}>
+                <div className={`${style.dialog} ${style.activeD}`}>
+                    William
+                </div>
+                <div className={style.dialog}>Rita</div>
+                <div className={style.dialog}>Leo</div>
+                <div className={style.dialog}>Mia</div>
+                <div className={style.dialog}>Monika</div>
+            </div>
+            <div className={style.messagesSection}>
+                <div className={style.message}>Hi, check it out!</div>
+                <div className={style.message}>Wow, nice to see you...</div>
+                <div className={style.message}>
+                    Where have you been all this time?
+                </div>
+                <div className={style.message}>I just arrived from Miami!</div>
+            </div>
+        </div>
+    )
 }
