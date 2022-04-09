0bbfdb3d53f613ae437703fd4768cff992c357bb
DoWhatYouL0ve <william_ross@mailfence.com>
2022-04-09 19:21:31 +0200

    lesson22 page Dialogs NavLink

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import style from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
         <div className={style.dialogsPageWrapper}>
             <div className={style.dialogsSection}>
-                <div className={`${style.dialog} ${style.activeD}`}>
-                    William
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/1">William</NavLink>
                 </div>
-                <div className={style.dialog}>Rita</div>
-                <div className={style.dialog}>Leo</div>
-                <div className={style.dialog}>Mia</div>
-                <div className={style.dialog}>Monika</div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/2">Rita</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/3">Leo</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/4">Mia</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/5">Monika</NavLink>
+                </div>
             </div>
             <div className={style.messagesSection}>
                 <div className={style.message}>Hi, check it out!</div>
