22c111d5992b28279866b792f2d91231a55cd145
DoWhatYouL0ve <william_ross@mailfence.com>
2022-05-28 19:17:49 +0200

    Lesson 53 Class component)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,58 +4,73 @@ import style from './users.module.css'
 import image from './../../images/ava.jpg'
 import axios from 'axios'
 
-export const Users = (props: UsersPropsType) => {
-    let getUsers = () => {
-        if (props.users.users.length === 0) {
+export class Users extends React.Component<UsersPropsType, any> {
+    constructor(props: UsersPropsType) {
+        super(props)
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    getUsers = () => {
+        if (this.props.users.users.length === 0) {
             axios
                 .get('https://social-network.samuraijs.com/api/1.0/users')
                 .then((response) => {
-                    props.setUsers(response.data.items)
+                    this.props.setUsers(response.data.items)
                 })
         }
     }
-
-    return (
-        <div>
-            <button onClick={getUsers}>Get Users</button>
-            {props.users.users.map((u) => (
-                <div key={u.id}>
-                    <span>
-                        <div>
-                            <img
-                                src={
-                                    u.photos.small !== null
-                                        ? u.photos.small
-                                        : image
-                                }
-                                alt=""
-                                className={style.userPhoto}
-                            />
-                        </div>
-                        <div>
-                            {u.followed ? (
-                                <button onClick={() => props.unfollow(u.id)}>
-                                    Unfollow
-                                </button>
-                            ) : (
-                                <button onClick={() => props.follow(u.id)}>
-                                    Follow
-                                </button>
-                            )}
-                        </div>
-                    </span>
-                    <span>
+    render() {
+        return (
+            <div>
+                {this.props.users.users.map((u) => (
+                    <div key={u.id}>
                         <span>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
+                            <div>
+                                <img
+                                    src={
+                                        u.photos.small !== null
+                                            ? u.photos.small
+                                            : image
+                                    }
+                                    alt=""
+                                    className={style.userPhoto}
+                                />
+                            </div>
+                            <div>
+                                {u.followed ? (
+                                    <button
+                                        onClick={() =>
+                                            this.props.unfollow(u.id)
+                                        }
+                                    >
+                                        Unfollow
+                                    </button>
+                                ) : (
+                                    <button
+                                        onClick={() => this.props.follow(u.id)}
+                                    >
+                                        Follow
+                                    </button>
+                                )}
+                            </div>
                         </span>
                         <span>
-                            <div>{'u.location.country'}</div>
-                            <div>{'u.location.city'}</div>
+                            <span>
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                            </span>
+                            <span>
+                                <div>{'u.location.country'}</div>
+                                <div>{'u.location.city'}</div>
+                            </span>
                         </span>
-                    </span>
-                </div>
-            ))}
-        </div>
-    )
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
